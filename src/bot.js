c8814f1cc6192db23c86cbf26c5b215de0975837
ebisuamagi <lhu59@ucsc.edu>
2021-10-13 18:51:47 -0700

    Added the bot.js function call examples

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,23 @@
-//Importing libraries
+/*
+  Importing Libraries
+    dotenv:       a required library to actually boot up the bot
+    examplelib:   a local library containing some example discordapi functions
+*/
 require('dotenv').config();
-const { Client, Intents} = require('discord.js');
+const examplelib = require("./exampleevents.js")
 
 /*
   Code to login the bot onto the servers.
+    client_obj:   a object of the Client class, the main hub for interacting with
+                  the discord API. https://discord.js.org/#/docs/main/stable/class/Client
+    
 */
-const client_obj = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]}); //Creating a client_object to use Client functions
-//console.log(process.env.DISCORDJS_BOT_TOKEN); //debug print statement,
-client_obj.login(process.env.DISCORDJS_BOT_TOKEN); //This code logs in our bot.
+const { Client, Intents } = require('discord.js');
+const client_obj = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+//Example function calls
+examplelib.bot_login_debug(client_obj);
+examplelib.bot_message_debug(client_obj);
+
+//This code logs in our bot.
+client_obj.login(process.env.DISCORDJS_BOT_TOKEN);
