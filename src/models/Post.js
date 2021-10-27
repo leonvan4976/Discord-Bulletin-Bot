d321b3a82dc34a5f63e9d13e2b3a2eb26dd02f8b
ebisuamagi <lhu59@ucsc.edu>
2021-10-27 11:41:08 -0700

    Added models folder

---
+++ src/models/Post.js
@@ -0,0 +1,21 @@
+
+//Creates the User Table
+
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define('post', {
+        id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            // Auto increment automatically sets the id value
+            autoIncrement: true,
+            // Has to be unique
+            unique: true,
+            validate: {
+                notEmpty: true
+            }
+        },
+        // Holds a foreign key to the owner of the post
+        userId: DataTypes.STRING,
+        messageContent: DataTypes.TEXT
+    })
+};
